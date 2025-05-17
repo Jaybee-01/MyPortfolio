705fb8d6fb07eb23c4f08aa5c11e587ada9771fb
Jaybee <emmnanueljoshua348@gmail.com>
2025-05-17 11:37:13 +0100

    almost done

--- scripts.js
+++ scripts.js
@@ -15,19 +15,15 @@ links.forEach((link) => {
   };
 });
 
-
 const form = document.getElementById("formInput");
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
   const myName = document.getElementById("myName").value.trim();
   const myEmail = document.getElementById("myEmail").value.trim();
   const myMessage = document.getElementById("myMessage").value.trim();
 
   if (!myName || !myEmail || !myMessage) {
     alert("Fields cannot be empty");
-  } else {
-    alert(`${myName} your message have been delivered`);
+    e.preventDefault();
   }
 });
